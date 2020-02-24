51dd01792b01a43c50094e2788dcee72b82fbd9e
maksimvorozcov <myks1992@mail.ru>
2020-02-25 02:59:54 +0300

    Added email testing

---
+++ tests/Unit/Model/User/Entity/User/EmailTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Myks92\User\Tests\Unit\Model\User\Entity\User;
+
+
+use InvalidArgumentException;
+use Myks92\User\Model\User\Entity\User\Email;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $email = new Email($value = 'email@app.test');
+
+        self::assertEquals($value, $email->getValue());
+    }
+
+    public function testCase(): void
+    {
+        $email = new Email('EmAil@app.test');
+
+        self::assertEquals('email@app.test', $email->getValue());
+    }
+
+    public function testIncorrect(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Email('not-email');
+    }
+
+    public function testEmpty(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Email('');
+    }
+}
