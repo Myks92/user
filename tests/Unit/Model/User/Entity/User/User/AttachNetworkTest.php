b701dc8e474fa78380b51c7984a84799f75d4220
maksimvorozcov <myks1992@mail.ru>
2020-02-25 07:08:18 +0300

    Added network testing entity

---
+++ tests/Unit/Model/User/Entity/User/User/AttachNetworkTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Myks92\User\Tests\Unit\Model\User\Entity\User\User;
+
+use Myks92\User\Model\User\Entity\User\Event\UserNetworkAttached;
+use Myks92\User\Model\User\Entity\User\Network;
+use Myks92\User\Tests\Builder\User\UserBuilder;
+use PHPUnit\Framework\TestCase;
+
+class AttachNetworkTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $user = (new UserBuilder())->active()->build();
+
+        $user->attachNetwork($network = 'vk', $identity = '0000001');
+
+        self::assertCount(1, $networks = $user->getNetworks());
+        self::assertInstanceOf(Network::class, $first = reset($networks));
+        self::assertEquals($identity, $first->getIdentity());
+        self::assertEquals($network, $first->getNetwork());
+
+        /** @var UserNetworkAttached $event */
+        $event = $user->releaseEvents()[2];
+
+        self::assertInstanceOf(UserNetworkAttached::class, $event);
+        self::assertEquals($user->getId(), $event->getId());
+        self::assertEquals($first->getNetwork(), $event->getNetwork());
+        self::assertEquals($first->getIdentity(), $event->getIdentity());
+    }
+
+    public function testAlready(): void
+    {
+        $user = (new UserBuilder())->active()->build();
+
+        $user->attachNetwork($network = 'vk', $identity = '0000001');
+
+        $this->expectExceptionMessage('Network is already attached.');
+        $user->attachNetwork($network = 'vk', $identity = '0000001');
+    }
+}
---
+++ tests/Unit/Model/User/Entity/User/User/DetachNetworkTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Myks92\User\Tests\Unit\Model\User\Entity\User\User;
+
+use Myks92\User\Model\User\Entity\User\Event\UserNetworkDetached;
+use Myks92\User\Model\User\Entity\User\Network;
+use Myks92\User\Tests\Builder\User\UserBuilder;
+use PHPUnit\Framework\TestCase;
+
+class DetachNetworkTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $user = (new UserBuilder())->viaNetwork($networkVk = 'vk', $identityVk = '0000001')->active()->build();
+
+        $user->attachNetwork($networkFacebook = 'facebook', $identityFacebook = '0000002');
+
+        self::assertCount(2, $networks = $user->getNetworks());
+
+        $user->detachNetwork($networkFacebook, $identityFacebook);
+
+        self::assertCount(1, $networks = $user->getNetworks());
+        self::assertInstanceOf(Network::class, $first = reset($networks));
+        self::assertEquals($networkVk, $first->getNetwork());
+        self::assertEquals($identityVk, $first->getIdentity());
+
+        /** @var UserNetworkDetached $event */
+        $event = $user->releaseEvents()[3];
+
+        self::assertInstanceOf(UserNetworkDetached::class, $event);
+        self::assertEquals($user->getId(), $event->getId());
+        self::assertEquals($networkFacebook, $event->getNetwork());
+        self::assertEquals($identityFacebook, $event->getIdentity());
+    }
+
+    public function testAlready(): void
+    {
+        $user = (new UserBuilder())->viaNetwork($network = 'vk', $identity = '0000001')->active()->build();
+
+        $this->expectExceptionMessage('Unable to detach the last identity.');
+        $user->detachNetwork($network, $identity);
+    }
+
+    public function testNotAttached(): void
+    {
+        $user = (new UserBuilder())->active()->build();
+
+        $this->expectExceptionMessage('Network is not attached.');
+        $user->detachNetwork('vk', '0000001');
+    }
+}
