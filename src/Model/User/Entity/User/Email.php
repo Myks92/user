993e39efa7fa8ff174e1696fab4dbf7275fe7dbe
maksimvorozcov <myks1992@mail.ru>
2020-03-03 04:58:08 +0300

    Updated check for email

--- src/Model/User/Entity/User/Email.php
+++ src/Model/User/Entity/User/Email.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Myks92\User\Model\User\Entity\User;
 
-use InvalidArgumentException;
 use Webmozart\Assert\Assert;
 
 /**
@@ -23,9 +22,7 @@ class Email
     public function __construct(string $value)
     {
         Assert::notEmpty($value);
-        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-            throw new InvalidArgumentException('Incorrect email.');
-        }
+        Assert::email($value, 'Incorrect email.');
         $this->value = mb_strtolower($value);
     }
 
